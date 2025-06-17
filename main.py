c42b9126adff61431de4a1adf843eedefbcb2765
ghDecapy <josh.mci112@gmail.com>
2025-06-17 11:12:50 +0100

    Frame has been rated

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
