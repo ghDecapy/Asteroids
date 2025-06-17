f7fb28f6fa6daf3cae0d60598a297f3a31a03ef9
ghDecapy <josh.mci112@gmail.com>
2025-06-17 13:25:58 +0100

    The 'oids are aster' us

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
-
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,8 +15,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
